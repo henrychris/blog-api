3f78f96a88cd4e7475f92de6633d5253afd50133
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-06-24 19:07:46 +0100

    add route for base API url

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -4,5 +4,6 @@ import { getIndex } from "../controllers/indexController";
 const indexRouter = express.Router();
 
 indexRouter.get("/", getIndex);
+indexRouter.get("/api", getIndex);
 
 export default indexRouter;
