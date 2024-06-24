90e203ba92ddf08075494c2d17586752c528f3fb
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-06-24 18:30:35 +0100

    add basic express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}.`);
+    console.log(`URL: http://localhost:${PORT}/`);
+});
