4bc725a67930a06ebfbcd78d8f4edb955b6f5e4b
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-06-24 18:43:59 +0100

    connect to mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { type Request, type Response } from "express";
+import * as mongoose from "mongoose";
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
+
+// mongoose setup
+await mongoose.connect("mongodb://localhost:27017/basic-blog-api");
+console.log("Connected to mongoose.");
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
@@ -12,3 +18,5 @@ app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}.`);
     console.log(`URL: http://localhost:${PORT}/`);
 });
+
+await mongoose.disconnect();
